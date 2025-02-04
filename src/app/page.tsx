24997a32df7ed3c420ca223f5fe9071053d500f2
DimDimich-creator <stilys62@gmail.com>
2025-02-04 19:48:43 +0300

    feat: add stars background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import { Button } from "@/components/ui/button";
+import { StarsBackground } from "@/components/stars-background";
 
 export default function Home() {
+  const settings = {
+    density: 0.00015,
+    allTwinkle: true,
+    twinkleProbability: 0.7,
+    minSpeed: 0.7,
+    maxSpeed: 1,
+  };
+
   return (
-    <div className="">
-      
+    <div className="min-h-screen w-full bg-black relative overflow-hidden ">
+      <StarsBackground
+        starDensity={settings.density}
+        allStarsTwinkle={settings.allTwinkle}
+        twinkleProbability={settings.twinkleProbability}
+        minTwinkleSpeed={settings.minSpeed}
+        maxTwinkleSpeed={settings.maxSpeed}
+      />
+      {/* <h1 className="text-white text-8xl font-bold text-center ">
+        Welcome to <span className="text-pink-400">AI Astro</span>
+      </h1> */}
+      {/* <p className="text-white text-2xl">A place for AI-powered astronomy</p> */}
     </div>
   );
 }
