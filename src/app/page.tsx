761f0204a0bd42e9d1dc9bc8fda31866f4ad1a16
DimDimich-creator <stilys62@gmail.com>
2025-02-06 18:51:01 +0300

    feat: add new components home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import { InteractiveHoverButton } from "@/components/interactive-hover-button";
+import { ShootingStars } from "@/components/shooting-stars";
+import { SplineScene } from "@/components/splite";
 import { StarsBackground } from "@/components/stars-background";
+import { siteConfig } from "@/site/config";
+
+export const settings = {
+  density: 0.00015,
+  allTwinkle: true,
+  twinkleProbability: 0.7,
+  minSpeed: 0.7,
+  maxSpeed: 1,
+};
 
 export default function Home() {
-  const settings = {
-    density: 0.00015,
-    allTwinkle: true,
-    twinkleProbability: 0.7,
-    minSpeed: 0.7,
-    maxSpeed: 1,
-  };
-
   return (
-    <div className="min-h-screen w-full bg-black relative overflow-hidden ">
+    <div className="h-screen w-full bg-background relative overflow-hidden ">
       <StarsBackground
         starDensity={settings.density}
         allStarsTwinkle={settings.allTwinkle}
@@ -18,10 +22,44 @@ export default function Home() {
         minTwinkleSpeed={settings.minSpeed}
         maxTwinkleSpeed={settings.maxSpeed}
       />
-      {/* <h1 className="text-white text-8xl font-bold text-center ">
-        Welcome to <span className="text-pink-400">AI Astro</span>
-      </h1> */}
-      {/* <p className="text-white text-2xl">A place for AI-powered astronomy</p> */}
+      <ShootingStars
+        starColor="#9E00FF"
+        trailColor="#2EB9DF"
+        minSpeed={15}
+        maxSpeed={35}
+        minDelay={1000}
+        maxDelay={3000}
+      />
+      <ShootingStars
+        starColor="#00FF9E"
+        trailColor="#00B8FF"
+        minSpeed={20}
+        maxSpeed={40}
+        minDelay={1500}
+        maxDelay={3500}
+      />
+      <div className="flex h-full">
+        {/* Left content */}
+        <div className="flex-1 p-8 relative z-10 flex flex-col justify-center items-center text-center">
+          <h1 className="text-4xl md:text-5xl xl:text-7xl font-bold">
+            {siteConfig.name}
+          </h1>
+          <p className="mt-4 text-neutral-300 max-w-xl">
+            {siteConfig.description}
+          </p>
+          <InteractiveHoverButton text="Journey" className="mt-8" />
+        </div>
+
+        {/* Right content */}
+        <div className="hidden md:block md:flex-1 relative">
+          <SplineScene
+            scene="https://prod.spline.design/bAnq74APLU5Vgdnk/scene.splinecode"
+            className="w-full h-full"
+            width={512}
+            height={512}
+          />
+        </div>
+      </div>
     </div>
   );
 }
