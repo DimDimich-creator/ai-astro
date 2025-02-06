ef384092015bfb716ad6aac8b81d590a7d85f104
DimDimich-creator <stilys62@gmail.com>
2025-02-06 18:47:01 +0300

    feat: add split component

---
+++ src/components/splite.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Suspense, lazy } from "react";
+const Spline = lazy(() => import("@splinetool/react-spline"));
+
+interface SplineSceneProps {
+  scene: string;
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+export function SplineScene({
+  scene,
+  className,
+  width,
+  height,
+}: SplineSceneProps) {
+  return (
+    <Suspense
+      fallback={
+        <div className="w-full h-full flex items-center justify-center">
+          <span className="loader" />
+        </div>
+      }
+    >
+      <Spline
+        scene={scene}
+        className={className}
+        // width={width}
+        // height={height}
+      />
+    </Suspense>
+  );
+}
